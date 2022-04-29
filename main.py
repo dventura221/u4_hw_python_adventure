e218f4756c1fa68c17bc648ae1a6ab1738be0e51
Dan Ventura <danice.ventura@gmail.com>
2022-04-29 12:48:30 -0600

    started

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Create your own adventure with python here
+
+def intro():
+    print('')
+    print('Welcome to your adventure, ' + name)
+    #print('What is your name?')
+
+
+name = input('What is your name?')
+intro()
